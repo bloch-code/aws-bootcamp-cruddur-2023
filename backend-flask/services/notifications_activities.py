dc21339bb333b53f232b8fe90af54f13be032914
bloch-code <dziurawyjoe@gmail.com>
2023-02-25 13:37:57 +0000

    changed a backend message

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -4,7 +4,7 @@ class NotificationsActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Too hot to handle',
+      'handle':  'TooHotToHandle',
       'message': 'AWS gang for life',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
@@ -14,8 +14,8 @@ class NotificationsActivities:
       'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Worf',
-        'message': 'This post has no honor!',
+        'handle':  'DaddyYankee',
+        'message': 'Woop woop it is working!',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
