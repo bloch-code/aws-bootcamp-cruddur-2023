e5cc3fa4c282cffaaf73b223e557a7209d5808ba
bloch-code <dziurawyjoe@gmail.com>
2023-03-07 20:19:05 +0000

    chanegs to app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -43,7 +43,7 @@ backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
 cors = CORS(
   app, 
-  resources={r"/api/*": {"origins": *}},
+  resources={r"/api/*": {"origins": "*"}},
   expose_headers="location,link",
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
