d18d005bd2eeb507ecd213a27d404a109a38f8ec
bloch-code <dziurawyjoe@gmail.com>
2023-05-09 22:32:45 +0000

    change Andres emails to my gmail test ones

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Andrew Brown','dziurawyjoe+test1@gmail.com' , 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko','dziurawyjoe+test2@gmail.com' , 'bayko' ,'MOCK'),
   ('Piotr Bloch', 'dziurawyjoe@gmail.com','pbloch','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
